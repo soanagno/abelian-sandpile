cf1ff5600d8ca586a3cb7aa1b2fcf652ddac3ba8
soanagno <65159898+soanagno@users.noreply.github.com>
2022-01-21 17:56:43 +0100

    tests

---
+++ test_automata.py
@@ -0,0 +1,106 @@
+import os
+import numpy as np
+import automata
+import matplotlib.pyplot as plt
+
+BASE_PATH = os.path.dirname(__file__)
+
+
+def test_sandpile():
+
+    initial64 = np.load(os.sep.join((BASE_PATH, 'pile_64x64_init.npy')))
+    final64 = np.load(os.sep.join((BASE_PATH, 'pile_64x64_final.npy')))
+
+    assert (automata.sandpile(initial64) == final64).all()
+
+    initial128 = np.load(os.sep.join((BASE_PATH, 'pile_128x128_init.npy')))
+    final128 = np.load(os.sep.join((BASE_PATH, 'pile_128x128_final.npy')))
+
+    assert (automata.sandpile(initial128) == final128).all()
+
+
+def test_life():
+
+    # pdf 5x5 glider case with periodic boundaries
+    # expected to be in the same place after 20 iterations
+    X = 0*np.arange(5*5).reshape(5, 5)
+    X[1, 1:4] = 1
+    X[2, 3] = 1
+    X[3, 2] = 1
+
+    assert (automata.life(X, 20, True) == X).all()
+
+
+def test_lifetri():
+
+    # pdf 10x10 triangular glider case with periodic boundaries
+    # expected to be in the same place after 30 iterations
+    X = 0*np.arange(10*10).reshape(10, 10)
+    X[8:10, 5:9] = 1
+
+    assert (automata.lifetri(X, 30, True) == X).all()
+
+
+def test_life_generic():
+
+    # pdf 5x5 glider case
+    # expected to translate diagonaly in 4 steps
+
+    # Calculate glider initial state
+    X = np.zeros(5*5).reshape(5, 5)
+    X[1, 1:4] = 1
+    X[2, 3] = 1
+    X[3, 2] = 1
+    XX = X.flatten()  # transform to 1D
+
+    # Calculate glider final state
+    X_final = np.zeros(5*5).reshape(5, 5)
+    X_final[0, 2:] = 1
+    X_final[1, -1] = 1
+    X_final[2, -2] = 1
+    XX_final = X_final.flatten()  # transform to 1D
+
+    # Adjacency matrix for 5x5 cartesian grid
+    # the .txt file has also been uploaded in the repo
+    M = np.array([
+        [0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0]])
+
+    # Assert that the 1D output of life_generic after 4 iterations
+    # is the same as the known final state of the glider
+
+    assert (automata.life_generic(M, XX, 4, {2, 3}, {3}) == XX_final).all()
+
+    # plt.matshow(M)
+    # plt.show()
+
+
+# Test cases
+
+test_sandpile()
+# test_life()
+# test_lifetri()
+# test_life_generic()
